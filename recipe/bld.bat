f3eb575845964d7af139f995d39b845a7232241d
Mark Kittisopikul <markkitt@gmail.com>
2023-06-11 17:03:37 -0400

    Try to fix Windows builds

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
 mamba create -yp .\bootstrap maven=3.9.1
 
-.\bootstrap\Library\bin\mvn -DdistributionTargetDir="%LIBRARY_PREFIX%" -Drat.ignoreErrors=true clean package
+.\bootstrap\Library\bin\mvn -DdistributionTargetDir="%LIBRARY_PREFIX%" -Drat.ignoreErrors=true -Dspotless.check.skip=true clean package
