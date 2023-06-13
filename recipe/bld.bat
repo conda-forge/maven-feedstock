26481d0506b48ac86f45686141730722b357a37c
Mark Kittisopikul <mkitti@users.noreply.github.com>
2023-06-13 14:42:41 -0400

    Fix Windows due to lib/ext (#25)
    * Restore removal of lib/ext
    
    * Bump build to 3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
 CALL mamba create -yp .\bootstrap maven=3.9.1
 
 CALL .\bootstrap\Library\bin\mvn -DdistributionTargetDir="%LIBRARY_PREFIX%" -Drat.ignoreErrors=true -Dspotless.check.skip=true -Dmaven.clean.failOnError=false clean package
+
+rd /s /q "%LIBRARY_PREFIX%\lib\ext\"
