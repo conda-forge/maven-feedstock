d28b7324bba43d49ed80c9ba39e5a715d705aa9d
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2023-06-12 21:16:38 -0400

    Disable fail on error during maven clean on Windows
    https://maven.apache.org/plugins/maven-clean-plugin/faq.html

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
 CALL mamba create -yp .\bootstrap maven=3.9.1
 
-CALL .\bootstrap\Library\bin\mvn -DdistributionTargetDir="%LIBRARY_PREFIX%" -Drat.ignoreErrors=true -Dspotless.check.skip=true clean package
+CALL .\bootstrap\Library\bin\mvn -DdistributionTargetDir="%LIBRARY_PREFIX%" -Drat.ignoreErrors=true -Dspotless.check.skip=true -Dmaven.clean.failOnError=false clean package
