7c43eebdae3ec06d60f0d18d4b36c2374e42dd4e
Mark Kittisopikul <mkitti@users.noreply.github.com>
2023-06-13 09:31:38 -0400

    Fix Windows builds, use call in bld.bat file (#24)
    * Try to fix Windows builds
    
    * Restrict openjdk to 17 or less
    
    * Use CALL in bat file
    
    * Disable fail on error during maven clean on Windows
    
    https://maven.apache.org/plugins/maven-clean-plugin/faq.html
    
    ---------
    
    Co-authored-by: Kittisopikul <kittisopikulm@hhmi.org>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
-mamba create -yp .\bootstrap maven=3.9.1
+CALL mamba create -yp .\bootstrap maven=3.9.1
 
-.\bootstrap\Library\bin\mvn -DdistributionTargetDir="%LIBRARY_PREFIX%" -Drat.ignoreErrors=true clean package
+CALL .\bootstrap\Library\bin\mvn -DdistributionTargetDir="%LIBRARY_PREFIX%" -Drat.ignoreErrors=true -Dspotless.check.skip=true -Dmaven.clean.failOnError=false clean package
